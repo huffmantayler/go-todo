a6e1286598151992133fd10ce66361f49c30835d
Taylor <huffmantayler@yahoo.com>
2025-03-09 21:44:49 -0400

    frontend changes

--- proj/frontend/src/components/TodoList.jsx
+++ proj/frontend/src/components/TodoList.jsx
@@ -33,7 +33,7 @@ export default function TodoList() {
   const getAllTodos = () => {
     axios.get(`${backendUrl}/getAllTodos`)
     .then(res => {
-      console.log("Frontend Changes") 
+      console.log("Frontend Changes2") 
       setTodos(res.data)
     })
     .catch(err => {
